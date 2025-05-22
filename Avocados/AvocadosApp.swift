8ed2f23f0cb4971d29f1e71a50265570b69334ce
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2025-05-22 15:47:16 -0500

    first commit

---
+++ Avocados/AvocadosApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvocadosApp.swift
+//  Avocados
+//
+//  Created by Isaac Urbina on 5/22/25.
+//
+
+import SwiftUI
+
+@main
+struct AvocadosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
